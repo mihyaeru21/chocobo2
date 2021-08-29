3ff446b3779621901ea11715a03ca2963af49f10
Mihyaeru <mihyaeru21@gmail.com>
2021-08-29 18:44:55 +0900

    :sparkles: 表示

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Box, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import ItemList from 'components/ItemList';
-import { Card, Item, unidentifiedCards } from 'logics/data';
+import { Card, Item, unidentifiedCards, cards } from 'logics/data';
+import { useEffect } from 'react';
 
 const initialItems: Array<Item<Card>> = unidentifiedCards.map((uc) => ({
   fuzzyName: uc,
@@ -12,7 +13,31 @@ const initialItems: Array<Item<Card>> = unidentifiedCards.map((uc) => ({
 }));
 
 export default function App() {
-  const [items, _setItems] = useState(initialItems);
+  const [items, setItems] = useState(initialItems);
+
+  // デバッグ用。あとで storybook を使えるとベストか？
+  useEffect(() => {
+    setItems([
+      {
+        fuzzyName: unidentifiedCards[0],
+        status: 'unidentified',
+        sellingPrice: null,
+        concreteItem: null,
+      },
+      {
+        fuzzyName: unidentifiedCards[1],
+        status: 'maybe',
+        sellingPrice: 250,
+        concreteItem: cards[8],
+      },
+      {
+        fuzzyName: unidentifiedCards[2],
+        status: 'identified',
+        sellingPrice: 50,
+        concreteItem: cards[10],
+      },
+    ]);
+  }, []);
 
   return (
     <ChakraProvider>
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Box, ChakraProvider, Container } from '@chakra-ui/react';
+import {
+  Badge,
+  Center,
+  Container,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import type { Item, Card } from 'logics/data';
 
 interface Props {
@@ -8,10 +18,51 @@ interface Props {
 
 export default function ItemList({ items }: Props) {
   return (
-    <Container>
-      {items.map((item) => (
-        <Box key={item.fuzzyName}>{item.fuzzyName}</Box>
-      ))}
+    <Container maxW="container.sm">
+      <Table size="sm">
+        <Thead>
+          <Tr>
+            <Th>
+              <Center>ステータス</Center>
+            </Th>
+            <Th>名前</Th>
+            <Th isNumeric>売値</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {items.map((item) => (
+            <Tr key={item.fuzzyName}>
+              <Td>
+                <Center>{statusBadge(item)}</Center>
+              </Td>
+              <Td>{viewName(item)}</Td>
+              <Td isNumeric>{item.sellingPrice ?? '???'}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Container>
   );
+}
+
+function statusBadge<T>(item: Item<T>): React.ReactNode {
+  switch (item.status) {
+    case 'identified':
+      return <Badge colorScheme="green">識別済</Badge>;
+    case 'maybe':
+      return <Badge colorScheme="blue">推測済</Badge>;
+    default:
+      return <Badge colorScheme="yellow">未識別</Badge>;
+  }
+}
+
+function viewName(item: Item<Card>): string {
+  switch (item.status) {
+    case 'identified':
+      return item.concreteItem?.name ?? '';
+    case 'maybe':
+      return `${item.fuzzyName}（${item.concreteItem?.name ?? ''}）`;
+    default:
+      return item.fuzzyName;
+  }
 }
